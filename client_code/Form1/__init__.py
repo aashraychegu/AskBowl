8807ab0deb2f916694740b33a0703167eca44a37
aashraychegu@hotmail.com <aashraychegu@hotmail.com>
2022-09-15 20:57:52 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,12 +4,18 @@ import plotly.graph_objects as go
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+import time
 class Form1(Form1Template):
 
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        print("1")
+        time.sleep(5)
+        print("2")
         self.init_components(**properties)
+        print(3)
+        time.sleep(5)
+        print(4)
 #     alert(title="hello")
 #     Notification(timeout=None, title="hello").show()
 
