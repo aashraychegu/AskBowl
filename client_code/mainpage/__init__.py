c09f51fd7507de8fefcbafbc82f5701b37636131
aashraychegu@hotmail.com <aashraychegu@hotmail.com>
2023-01-30 18:11:10 +0000

    Created mainpage and mainpage.RowTemplate1; Edited settings; Deleted Form1.RowTemplate1 and Form1

--- client_code/mainpage/__init__.py
+++ client_code/mainpage/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import mainpageTemplate
 from anvil import *
 import plotly.graph_objects as go
 import anvil.tables as tables
@@ -6,17 +6,19 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import time
 from anvil.js.window import speechSynthesis as synth
+from anvil.js.window import alert, confirm, prompt
 
 
     
-class Form1(Form1Template):
-    
+class mainpage(mainpageTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        synth.getVoices()[0]
+        print(alert("hello"))
+        print(confirm("hi"))
+        print(prompt("uga buga"))
         # Any code you write here will run when the form opens.
-    def say(self,text,voice = synth.getVoices()[0],volume = 100,rate = 100,pitch = 100):
+    def say(self,text,voice = "",volume = 100,rate = 100,pitch = 100):
         text = text.replace("`","")
         utr = anvil.js.window.SpeechSynthesisUtterance(text)
         utr.voice = voice
@@ -26,5 +28,24 @@ class Form1(Form1Template):
         synth.speak(utr)
     def read_question_click(self, **event_args):
         """This method is called when the button is clicked"""
-        self.say("hi")
+        # self.say("hi")
+
+    def rate_pressed_enter(self, **event_args):
+        """This method is called when the user presses Enter in this text box"""
+        pass
+
+    def pause_reading_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+    def resume_reading_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        pass
+
+
+
+
+
+
+
 
